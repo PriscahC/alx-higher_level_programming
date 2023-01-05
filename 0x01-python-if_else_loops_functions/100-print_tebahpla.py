fb81cdba2743cbf318da048262924d55763b0065
Priscila A <105573463+PriscahC@users.noreply.github.com>
2023-01-05 04:22:30 -0800

    ASCII alphabet, in reverse order, upper, lower

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,1 +1,6 @@
-0x01. Python - if/else, loops, functions
+#!/usr/bin/python3
+for i in range(122, 96, -1):
+    asciToLetter = chr(i - 32)
+    if i % 2 == 0:
+        asciToLetter = chr(i)
+    print("{}".format(asciToLetter), end='')
