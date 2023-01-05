ac83a5a72cd9776c1db72ac16319f40890d4838e
Priscila A <105573463+PriscahC@users.noreply.github.com>
2023-01-05 04:23:50 -0800

    creates a copy of the string

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,1 +1,7 @@
-0x01. Python - if/else, loops, functions
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    if n < 0:
+        print(str, end='')
+    else:
+        print("{}".format(str[:n] + str[n+1:]), end='')
+    return ''
