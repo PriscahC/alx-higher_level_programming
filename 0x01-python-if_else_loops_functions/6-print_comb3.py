fef95cf1774f4b7ecc1067e874ba9ed8b07a70d2
Priscila A <105573463+PriscahC@users.noreply.github.com>
2023-01-05 04:12:37 -0800

    prints all combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,6 @@
-0x01. Python - if/else, loops, functions
+#!/usr/bin/python3
+for i in range(8):
+    for j in range(i + 1, 10):
+        print("{}".format(i), end='')
+        print("{}, ".format(j), end='')
+print("89")
