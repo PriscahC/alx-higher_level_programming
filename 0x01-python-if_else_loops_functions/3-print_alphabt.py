307d2b30e9ff1e1d85a493d86cb6c86e69747fd6
Priscila A <105573463+PriscahC@users.noreply.github.com>
2023-01-05 03:48:41 -0800

    prints ASCII alphabet, in lowercase

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,1 +1,6 @@
-0x01. Python - if/else, loops, functions
+#!/usr/bin/python3
+for i in range(97, 123):
+    asciToLetter = chr(i)
+    if i == 101 or i == 113:
+        continue
+    print("{}".format(asciToLetter), end='')
