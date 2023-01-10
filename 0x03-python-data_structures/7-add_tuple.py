c52b6322c055e2ea1e98cd74137eab438c3a3e9f
Priscila A <105573463+PriscahC@users.noreply.github.com>
2023-01-10 15:40:27 -0800

    adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,7 @@
-0x03-python-data_structures 0-print_list_integer.py 1-element_at.py 2-replace_in_list.py 3-print_reversed_list_integer.py 4-new_in_list.py 5-no_c.py 6-print_matrix_integer.py
+cat > #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    new_tuple = ()
+    tuple_1 = tuple_a + (0, 0)
+    tuple_2 = tuple_b + (0, 0)
+    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
+    return new_tuple
